e2fbccbb8f9ef699dbdba4c76a992ba437a13b25
Matt London <mrl2534@rit.edu>
2023-06-28 22:29:18 -0400

    Add test for current state

--- sudoku-solver/src/main.cpp
+++ sudoku-solver/src/main.cpp
@@ -2,8 +2,25 @@
 
 #include "Sudoku.hpp"
 
+void printArray(std::array<int, 9>& arr) {
+    for (int item: arr) {
+        std::cout << item << ", ";
+    }
+    std::cout << std::endl;
+
+}
+
 int main(void) {
-    std::cout << "Hello World!" << std::endl;
+    Sudoku board("../input/input1.txt");
+
+    board.print();
+
+    std::array<int, 9> tmp = board.getCol(3);
+    printArray(tmp);
+    tmp = board.getRow(5);
+    printArray(tmp);
+    tmp = board.getSubsquare(8, 7);
+    printArray(tmp);
 
     return 0;
 }
